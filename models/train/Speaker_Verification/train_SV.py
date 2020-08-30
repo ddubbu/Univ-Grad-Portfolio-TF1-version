1ff405ef13cf8d177d6a8d1cee2c3bd94273bc24
ddubbu <hello.ddubbu@gmail.com>
2020-08-30 23:02:57 +0900

    8-layer DNN fail because loss=3.00 remain

--- models/train/Speaker_Verification/train_SV.py
+++ models/train/Speaker_Verification/train_SV.py
@@ -24,7 +24,7 @@ from sklearn.preprocessing import MinMaxScaler
 # tf.set_random_seed(777)
 
 learning_rate = 0.001
-training_epochs = 15
+training_epochs = 20
 total_batch_size = 225
 keep_prob = tf.placeholder(tf.float32, name="keep_prob")
 sd = 1 / np.sqrt(13)  # standard deviation 표준편차(표본표준편차라 1/root(n))
@@ -38,24 +38,74 @@ X = tf.placeholder(tf.float32, [None, Tx, num_mfcc], name="X")
 Y = tf.placeholder(tf.float32, [None, num_class], name="Y")
 
 
-_X = tf.reshape(X, shape=[-1, Tx*num_mfcc])
+_X = tf.reshape(X, shape=[-1, Tx, num_mfcc, 1])  # for 2D conv
 
+CW1 = tf.Variable(tf.random_normal([3, 3, 1, 1], stddev=0.01))
+C1 = tf.nn.conv2d(_X, CW1, strides=[1, 2, 2, 1], padding="SAME")
+P1 = tf.nn.max_pool(C1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID")
 
+# print(P1.shape.as_list())
+# exit()
+P1 = tf.reshape(P1, shape=[-1, 43*6])
 
-
-
-
-
+# 1차 히든레이어
 W1 = tf.get_variable("w1",
-                     #tf.random_normal([Tx*num_mfcc, num_class], mean=0, stddev=sd),)
-                     shape=(Tx*num_mfcc, num_class),
+                     # tf.random_normal([216, 180], mean=0, stddev=sd),
+                     shape=[43*6, 256],
                      initializer=tf.contrib.layers.xavier_initializer())
-b1 = tf.Variable(tf.zeros([num_class]))
-# b1 = tf.Variable(tf.random_normal([num_class], mean=0, stddev=sd), name="b1")
-logits = tf.add(tf.matmul(_X, W1), b1) # hypothesis = tf.nn.sigmoid(tf.add(tf.matmul(_X, W1), b1))
-logits = tf.nn.dropout(logits, keep_prob=keep_prob)
-hypothesis = tf.nn.softmax(logits)
+b1 = tf.ones([256])
+L1 = tf.nn.relu(tf.matmul(_X, W1) + b1)  # 1차 히든레이어는 'Relu' 함수를 쓴다.
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 
+# 2차 히든 레이어
+W2 = tf.get_variable("w2",
+                     # tf.random_normal([180, 150], mean=0, stddev=sd),
+                     shape=[256, 256],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.ones([256])
+L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)  # 2차 히든레이어는 'Relu' 함수를 쓴다.
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
+
+
+# 4차 히든 레이어
+W4 = tf.get_variable("w4",
+                     # tf.random_normal([100, 50], mean=0, stddev=sd),
+                     shape=[256, 128],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.ones([128])
+L4 = tf.nn.relu(tf.matmul(L2, W4) + b4)  # 4차 히든레이어는 'Relu' 함수를 쓴다.
+L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
+
+# 5차 히든 레이어
+W5 = tf.get_variable("w5",
+                     # tf.random_normal([100, 50], mean=0, stddev=sd),
+                     shape=[128, 128],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.ones([128])
+L5 = tf.nn.relu(tf.matmul(L4, W5) + b5)  # 5차 히든레이어는 'Relu' 함수를 쓴다.
+L5 = tf.nn.dropout(L5, keep_prob=keep_prob)
+
+# 6차 히든 레이어
+W6 = tf.get_variable("w6",
+                     # tf.random_normal([100, 50], mean=0, stddev=sd),
+                     shape=[128, 128],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b6 = tf.ones([128])
+L6 = tf.nn.relu(tf.matmul(L5, W6) + b6)  # 6차 히든레이어는 'Relu' 함수를 쓴다.
+L6 = tf.nn.dropout(L6, keep_prob=keep_prob)
+
+
+# 최종 레이어
+W8 = tf.get_variable("w8",
+                     # tf.random_normal([50, num_output], mean=0, stddev=sd),
+                     shape=[128, num_class],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b8 = tf.ones([num_class])
+
+
+logits = tf.add(tf.matmul(L6, W8), b8)
+
+hypothesis = tf.nn.softmax(logits)
 cost = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y, name='myCost')
 cost = tf.reduce_mean(cost)
 
@@ -111,7 +161,7 @@ with tf.Session() as sess:
                 min_cost = cost_
                 saver.save(sess, './models/NN_cost_%.6f_epoch_%d'%(cost_, batch))
 
-    is_correct = tf.equal(tf.arg_max(hypothesis, 1), tf.arg_max(Y, 1))
+    # is_correct = tf.equal(tf.arg_max(hypothesis, 1), tf.arg_max(Y, 1))
     correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
@@ -143,14 +193,15 @@ with tf.Session() as sess:
     #print(np.shape(X_test))
     #print(np.shape(Y_test))
 
+    print(sess.run(hypothesis, feed_dict={X: X_test, keep_prob: 1}))
     print("predict idx: ", end='')
     print(sess.run(tf.argmax(hypothesis, 1), feed_dict={X: X_test, keep_prob: 1}))
     # print(pd.value_counts(pd.Series(sess.run(tf.argmax(hypothesis, 1),
     #                                         feed_dict={X: X_test, keep_prob: 1}))))
 
     ## 여러개일 때 유용
-    correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    # correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+    # accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     print("Accuracy: ", sess.run(accuracy, feed_dict={X: X_test, Y: Y_test, keep_prob: 1}))
 
 
